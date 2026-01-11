0b1432437ae151cc06fe34496e7e18b23ebe4c5e
PavolBrichta <pavol.brichta@gmail.com>
2026-01-11 18:14:55 +0100

    contacts

--- Dreziny/dreziny/src/pages/Contact.tsx
+++ Dreziny/dreziny/src/pages/Contact.tsx
@@ -8,7 +8,20 @@ export default function Contact() {
 			<h2>{t('contactTitle')}</h2>
 			<p>
 				{t('contactDescription')}{' '}
-				<a href="mailto:hello@example.com">briketaraketa@gmail.com</a>.
+				<a href="mailto:hello@example.com">briketaraketa@gmail.com</a>
+				{' '}{t('orCall')}{' '}<a href="tel:+421 908 082 881">0908 082 881</a>.
+			</p>
+			<p>
+				{t('location')}
+			</p>
+			<p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19920.38180563555!2d17.409322297075498!3d48.60766917681548!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476cb165b8992475%3A0x3aa634c89219e9db!2s906%2032%20Jablonica!5e0!3m2!1ssk!2ssk!4v1768150400631!5m2!1ssk!2ssk" width="600" height="450" loading="lazy"></iframe>
+			</p>
+			<p>
+				{t('openingHours')}
+			</p>
+			<p>
+				{t('price')}28,50 €.
 			</p>
 		</section>
 	)
--- Dreziny/dreziny/src/pages/Dashboard.tsx
+++ Dreziny/dreziny/src/pages/Dashboard.tsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import { useLanguage } from '../i18n/LanguageContext'
+//import { useLanguage } from '../i18n/LanguageContext'
 
 export default function Dashboard() {
-  const [count, setCount] = useState(0)
-  const { t } = useLanguage()
+  //const { t } = useLanguage()
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Vitajte v Drezinách!</h1>
+        <br/>
+        Kedze neviem co sem dat, necham to takto.
       </div>
-      <h1>{t('viteReact')}</h1>
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)}>
-          {t('countIs')} {count}
-        </button>
-        <p>{t('editCode')}</p>
-      </div>
-      <p className="read-the-docs">{t('clickLogos')}</p>
     </>
   )
 }
