eb7b583940b5672c3a48b8214dfc879510e7fbab
PavolBrichta <pavol.brichta@gmail.com>
2026-01-18 01:03:02 +0100

    dashboard description

--- Dreziny/dreziny/src/pages/Dashboard.tsx
+++ Dreziny/dreziny/src/pages/Dashboard.tsx
@@ -6,9 +6,28 @@ export default function Dashboard() {
   return (
     <section className="page">
       <div>
-        <h1>Vitajte na Drezinách!</h1>
-        <br/>
-        Kedze neviem co sem dat, necham to takto.
+        <h1>Hľadáte netradičnú zábavu?</h1>
+        <div>
+          Vyskúšajte jazdu na drezine! Dreziny sú skvelým spôsobom, ako stráviť
+          čas v prírode s rodinou a priateľmi. Objavte krásy okolia z inej
+          perspektívy a užite si nezabudnuteľný zážitok.
+        </div>
+        <div>
+          Rezervujte si svoju drezinu ešte dnes a pripravte sa na dobrodružstvo
+          plné zábavy a smiechu!
+        </div>
+        <div>
+          Naša ponuka drezín je vhodná pre všetky vekové kategórie a zaručuje
+          bezpečnú a pohodlnú jazdu.
+        </div>
+        <div>
+          Naše dreziny sú stavané tak, aby poskytovali maximálny komfort a bezpečnosť
+          počas jazdy. S našimi drezinami môžete objavovať malebné trasy a užívať
+          si krásy prírody.
+        </div>
+        <div>
+          Tešíme sa na vašu návštevu a prajeme vám príjemnú jazdu na drezine!
+        </div>
       </div>
     </section>
   )
