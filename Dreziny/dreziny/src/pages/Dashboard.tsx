1729fe570cde8c6f1d3f3c1b2aa07e6e88eff13d
PavolBrichta <pavol.brichta@gmail.com>
2026-01-28 22:45:18 +0100

    dashboard background

--- Dreziny/dreziny/src/pages/Dashboard.tsx
+++ Dreziny/dreziny/src/pages/Dashboard.tsx
@@ -5,25 +5,27 @@ export default function Dashboard() {
   const { t } = useLanguage()
 
   return (
-    <section className="page">
-      <div>
-        <h1>{t('homeTitle')}</h1>
-        <div className='description-paragraph'>
-          {t('homeDescription1')}
+    <div className='dashboard-container'>
+      <section className="page">
+        <div>
+          <h1>{t('homeTitle')}</h1>
+          <div className='description-paragraph'>
+            {t('homeDescription1')}
+          </div>
+          <div className='description-paragraph'>
+            {t('homeDescription2')}
+          </div>
+          <div className='description-paragraph'>
+            {t('homeDescription3')}
+          </div>
+          <div className='description-paragraph'>
+            {t('homeDescription4')}
+          </div>
+          <div className='description-paragraph'>
+            {t('homeDescription5')}
+          </div>
         </div>
-        <div className='description-paragraph'>
-          {t('homeDescription2')}
-        </div>
-        <div className='description-paragraph'>
-          {t('homeDescription3')}
-        </div>
-        <div className='description-paragraph'>
-          {t('homeDescription4')}
-        </div>
-        <div className='description-paragraph'>
-          {t('homeDescription5')}
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
